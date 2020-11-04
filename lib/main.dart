acf0253b8bf0ee636d2999f326586f984232e811
kiendn1999 <57392834+kiendn1999@users.noreply.github.com>
2020-11-04 21:19:21 +0700

    first

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ class MyApp extends StatelessWidget {
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        //kien dp trai
       ),
       home: SplashPage(),
     );
